9f3732ef99bfaababe19ce245d646ceeb38ee666
pauloalvesm <paulo.alves7351@gmail.com>
2025-10-25 12:47:28 -0300

    feat: add class for dependency injection

---
+++ src/CNAB.Infra.IoC/Configurations/DependencyInjection.cs
@@ -0,0 +1,18 @@
+using CNAB.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CNAB.Infra.IoC.Configurations;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(connectionString));
+        
+        return services;
+    }
+}
--- src/CNAB.WebAPI/Program.cs
+++ src/CNAB.WebAPI/Program.cs
@@ -1,4 +1,8 @@
+using CNAB.Infra.IoC.Configurations;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddInfrastructure(builder.Configuration);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
