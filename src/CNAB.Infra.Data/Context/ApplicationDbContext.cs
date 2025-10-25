02db51eb7e22d66e3d7df7776dc74de822937417
pauloalvesm <paulo.alves7351@gmail.com>
2025-10-25 12:14:09 -0300

    feat: initial configuration of the context with the database

---
+++ src/CNAB.Infra.Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+using CNAB.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CNAB.Infra.Data.Context;
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<Store> Stores { get; set; }
+    public DbSet<Transaction> Transactions { get; set; }
+
+    public ApplicationDbContext() { }
+
+    public ApplicationDbContext(DbContextOptions options) : base(options) { }
+}
